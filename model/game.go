4854de6b767fdaca8e8195f199191844571eedbd
shooooooma415 <shoma20040415@icloud.com>
2024-12-18 10:36:28 +0900

    fix:不要なtypeの削除

--- model/game.go
+++ model/game.go
@@ -2,17 +2,9 @@ package model
 
 type ImageId int
 
-type ImageName string
-
-type Image string //base64で画像をやり取りするから文字列
-
-type CompareImage string //これってImageと分ける必要あるんですかね？？
-
-type CompareImageName int //これも分けないでいいかも
-
 type Ranking struct{
 	Position int
 	UserId UserId
-	UserName UserName
-	FaceImage Image
+	UserName string
+	FaceImage string
 }
--- model/users.go
+++ model/users.go
@@ -1,5 +1,3 @@
 package model
 
 type UserId int
-
-type UserName string
