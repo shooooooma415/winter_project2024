cf50b59e86cf9b099e985920865964dcaf1ddef5
shooooooma415 <shoma20040415@icloud.com>
2024-12-20 16:11:43 +0900

    fix:インターフェースの切り分け

---
+++ repository/users.go
@@ -0,0 +1,10 @@
+package repository
+
+import (
+	"winter_pj/model"
+)
+
+type UsersRepository interface {
+	CreateUser(name model.UserName) (*model.UserId, error)
+	UpdateUser(userId model.UserId, name model.UserName) (*model.User, error)
+}
--- repository/users_sql.go
+++ repository/users_sql.go
@@ -6,11 +6,6 @@ import (
 	"winter_pj/model"
 )
 
-type UsersRepository interface {
-	CreateUser(name model.UserName) (*model.UserId, error)
-	UpdateUser(userId model.UserId, name model.UserName) (*model.User, error)
-}
-
 type UsersSQL struct {
 	DB *sql.DB
 }
