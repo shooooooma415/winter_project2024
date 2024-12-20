1868f6b87a6d2d93dff9a146392607405a0571f4
shooooooma415 <shoma20040415@icloud.com>
2024-12-20 11:21:42 +0900

    delete:コメントアウトの削除

--- repository/game_sql.go
+++ repository/game_sql.go
@@ -4,7 +4,6 @@ import (
 	"winter_pj/model"
 )
 
-// 実行結果boolで表すんならerrorいらない？いいやり方がある？
 type GameRepository interface {
 	GetCompareImage(searchName string) (*model.ImageList, error)
 	InsertCompareImage(imageName string, image string, isHuman bool) (error)
--- repository/rooms_sql.go
+++ repository/rooms_sql.go
@@ -4,7 +4,6 @@ import (
 	"winter_pj/model"
 )
 
-// 実行結果boolで表すんならerrorいらない？いいやり方がある？
 type RoomsRepository interface {
 	CreateRoom(isHuman bool, password model.Password, userId model.UserId) (*model.RoomId, error)
 	JoinRoom(password model.Password, userId model.UserId) (*model.RoomId, error)
