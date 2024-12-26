a12f6ac4430f36bee6feffbed96f9aaca747814c
shooooooma415 <shoma20040415@icloud.com>
2024-12-26 13:51:33 +0900

    add:interfaceにDeleteRoomMemberTableを追加

--- repository/room/room.go
+++ repository/room/room.go
@@ -10,5 +10,6 @@ type RoomRepository interface {
 	GetRoomName(roomId model.RoomId) (*model.RoomName, *model.RoomId, error)
 	GetAuthorId(roomId model.RoomId) (*model.UserId, *model.RoomId, error)
 	DeleteRoom(roomId model.RoomId) (*model.RoomId, error)
+	DeleteRoomMemberTable(roomId model.RoomId)(*model.RoomId, error)
 	WithdrawRoom(roomMember model.RoomMember) (*model.RoomMember, error)
 }
